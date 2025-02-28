2985fd36b0b012962700f52edd49846cacefd089
Lucas Almeida de Lima <allmeida.lima@gmail.com>
2025-02-27 21:12:38 -0300

    ajuste de mensagens

--- hunter-bot.py
+++ hunter-bot.py
@@ -3,47 +3,50 @@ import requests
 from emoji import emojize as e
 from sanitizer import clean_plates, verify_plates
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-
+# Carregar configurações do arquivo JSON
 with open('appsettings.json', 'r') as file:
     c = json.load(file)
-    
+
 async def fetch_data_from_api(query: str) -> str:
     url = c['apiUrl'].replace('{query}', query)   
     response = requests.get(url, verify=False) 
-    
-    if response.status_code == 200 and data and isinstance(data, dict) and 'plate' in data and data['plate'] != None:
+
+    # Obter JSON da resposta
+    if response.status_code == 200:
         data = response.json()
-        message = (
-            f"{e(c['emoji']['car'])}{e(c['emoji']['light'])}Placa Localizada.{e(c['emoji']['car'])}{e(c['emoji']['light'])}\n"
-            f"{data['plate']}\n"
-            f"{data['model']}\n"
-            f"{data['company']}\n"
-            f"Contato:{data['fone']}\n"
-            f"{e(c['emoji']['spy']) * 3}\n"
-        )
-        return message
-    elif response.status_code == 200 and str(response.json()) != 'null' and 'plate' not in response.json():
-        return f"{e(c['emoji']['expression'])}Placa não encontrada.{e(c['emoji']['expression'])}"
-    else:
-        return "Erro ao acessar a API."
+        
+        if data and isinstance(data, dict) and 'plate' in data and data['plate'] is not None:
+            return (
+                f"🚗🚨 Placa Localizada 🚗🚨\n"
+                f"🛑 **Placa:** {data['plate']}\n"
+                f"🚗 **Modelo:** {data['model']}\n"
+                f"🏢 **Empresa:** {data['company']}\n"
+                f"📞 **Contato:** {data['fone']}\n"
+                f"🕵️🕵️🕵️"
+            )
+        else:
+            return "😑 Placa não encontrada. 😑"
+
+    return "🚫 Erro ao acessar a API. 🚫"
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_message = update.message.text
     plate = clean_plates(user_message)
 
     if not verify_plates(plate):
-        await update.message.reply_text(f"{e(c['emoji']['error'])}Placa inválida.{e(c['emoji']['error'])}")
+        await update.message.reply_text("🚫 Placa inválida. 🚫")
         return
     
     result = await fetch_data_from_api(plate)
-    await update.message.reply_text(result)
+    await update.message.reply_text(result, parse_mode="Markdown")
 
+# Criar aplicação do bot
+app = ApplicationBuilder().token(c['botToken']).build()
 
-
-app = ApplicationBuilder().token(f"{c['botToken']}").build()
-
+# Adicionar handler para mensagens de texto
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
+# Rodar bot
 app.run_polling()
--- sanitizer.py
+++ sanitizer.py
@@ -2,10 +2,9 @@ import re
 
 
 def clean_plates(plates: str) -> str:
-    if re.match(r'[-\/*@ %$#&?]', plates):
-        plates = plates.replace('-', '').replace('@', '').replace('%', '').replace('$', '').replace('#', '').replace('&', '').replace('?', '')
-    return plates
+    return re.sub(r'[-\/*@ %$#&?()+=]', '', plates)
 
 def verify_plates(plates: str) -> bool:
-    re.match(r'^[a-zA-Z]{3}[0-9][A-Za-z0-9][0-9]{2}$', plates)
-    return True
+    if re.match(r'^[a-zA-Z]{3}[0-9][A-Za-z0-9][0-9]{2}$', plates) and len(plates) == 7:
+        return True
+    return False
